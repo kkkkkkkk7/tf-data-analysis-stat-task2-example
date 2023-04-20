acb400c7d7663c78aef04219810eaa5ef4512dc7
kkkkkkkk7 <128353867+kkkkkkkk7@users.noreply.github.com>
2023-04-20 14:23:21 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,14 +4,11 @@ import numpy as np
 from scipy.stats import norm
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 750949272 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+   level_of_trust = 0.8
+b = 5
+size_of_array = 10
+significance_levels = np.random.uniform(low=0.092, high=b, size=size_of_array)
+   num_greater = np.sum(significance_levels > level_of_trust)
